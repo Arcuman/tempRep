196f99ae81ac987715421d5f8bb7ff8d3f4d9dea
Arcuman <anton.arcuman.borisov@gmail.com>
2020-03-15 11:44:09 +0300

    Bug

---
+++ Borto/Borto/AttachedProperties/ButtonAttachedProperties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Borto
+{
+    
+    /// <summary>
+    /// The IsBusy  attached propery for a anything that wants to flag if the control is busy
+    /// </summary>
+    public class IsBusyProperty : BaseAttachedProperty<IsBusyProperty, bool>
+    {
+       
+    } 
+
+}
